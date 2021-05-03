cfbe591a6b133fc0c5c94de44a35092b2e97a493
YX H <2463143134@qq.com>
2021-05-03 19:31:02 +0800

    添加项目文件。

---
+++ ChatClient/client.cpp
@@ -0,0 +1,164 @@
+/*
+        一个简单的聊天客户端
+*/
+
+#include "chat_message.hpp"
+#include "SerilizationObject.hpp"
+
+#include <boost/asio.hpp>
+
+#include <deque>
+#include <iostream>
+#include <thread>
+
+#include <cstdlib>
+
+using boost::asio::ip::tcp;
+
+using chat_message_queue = std::deque<chat_message>;
+
+class chat_client {
+public:
+  chat_client(boost::asio::io_context &io_context,
+              const tcp::resolver::results_type &endpoints)
+      : io_context_(io_context), socket_(io_context) {
+    do_connect(endpoints);
+  }
+
+  void write(const chat_message &msg) {
+    boost::asio::post(io_context_, [this, msg]() {
+      bool write_in_progress = !write_msgs_.empty();
+      write_msgs_.push_back(msg);
+      if (!write_in_progress) {
+        // 如果程序不在写的过程中，执行do_write()
+        do_write();
+      }
+    }); // post：将事件注册进io_context，由io_context调度（事件在io_context.run()所在线程执行，保证线程安全）
+  }
+
+  void close() {
+    boost::asio::post(io_context_, [this]() { socket_.close(); });
+  }
+
+private:
+  void do_connect(const tcp::resolver::results_type &endpoints) {
+    boost::asio::async_connect(
+        socket_, endpoints,
+        [this](boost::system::error_code ec, tcp::endpoint) {
+          if (!ec) {
+            do_read_header();
+          }
+        });
+  }
+
+  void do_read_header() {
+    boost::asio::async_read(
+        socket_,
+        boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec && read_msg_.decode_header()) {
+            //std::cout << "读取包头成功！\n";
+            do_read_body();
+          } else {
+            socket_.close(); // 出错，关闭socket连接
+          }
+        });
+  }
+
+  void do_read_body() {
+    boost::asio::async_read(
+        socket_, boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec) {
+            //std::cout << "读取包体成功！\n";
+            if (read_msg_.type() == MT_ROOM_INFO) {
+              
+              SRoomInfo info;   // 服务端发来的消息
+              std::stringstream ss( // 将read_msh_构造string流
+                  std::string(read_msg_.body(),
+                              read_msg_.body() + read_msg_.body_length()));
+              boost::archive::text_iarchive oa(ss); //  使用text_iarchive 将ss流进行序列化赋值
+              oa &info;
+              std::cout << "client: '";
+              std::cout << info.name();
+              std::cout << "' says '";
+              std::cout << info.information();
+              std::cout << "'\n";
+            }
+            do_read_header();
+            /*std::cout.write(read_msg_.body(), read_msg_.body_length());
+            std::cout << "\n";
+            do_read_header();*/
+          } else {
+            std::cout << "error: " << ec << std::endl;
+            socket_.close();
+          }
+        });
+  }
+
+  void do_write() {
+    boost::asio::async_write(
+        socket_,
+        boost::asio::buffer(write_msgs_.front().data(),
+                            write_msgs_.front().length()),
+        [this](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec) {
+            write_msgs_.pop_front();
+            if (!write_msgs_.empty()) {
+              //std::cout << "消息发送成功！\n";
+              do_write();
+            }
+          } else {
+            socket_.close();
+          }
+        });
+  }
+
+private:
+  boost::asio::io_context &io_context_;
+  tcp::socket socket_;
+  chat_message read_msg_;
+  chat_message_queue write_msgs_;
+};
+
+int main(int argc, char *argv[]) {
+  try {
+    if (argc != 3) {
+      std::cerr << "Usage: chat_client <host> <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_context io_context;
+
+    tcp::resolver resolver(io_context);
+    auto endpoints = resolver.resolve(argv[1], argv[2]);
+    chat_client c(io_context, endpoints);
+
+    std::thread t([&io_context]() { io_context.run(); }); // 另开线程跑io
+
+    char line[chat_message::max_body_length + 1];
+    while (std::cin.getline(line, chat_message::max_body_length + 1)) {
+      chat_message msg;
+      auto type = 0;
+      std::string input(line, line + std::strlen(line));
+      std::string output;
+      if (parseMessage(input, &type, output)) {
+        msg.setMessage(type, output.data(), output.size());
+        c.write(msg);
+        std::cout << "write message for server " << output.size()
+                  << std::endl;
+      }
+      /*msg.body_length(std::strlen(line));
+      std::memcpy(msg.body(), line, msg.body_length());
+      msg.encode_header();
+      c.write(msg);*/
+    }
+
+    c.close(); // 释放相关资源，让io_context.run()退出
+    t.join();
+  } catch (std::exception &e) {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
---
+++ ChatServer/server.cpp
@@ -0,0 +1,252 @@
+/*
+        一个简单的聊天服务器
+*/
+#include "chat_message.hpp"
+#include "SerilizationObject.hpp"
+
+#include <boost/asio.hpp>
+
+#include <deque>
+#include <iostream>
+#include <list>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <utility>
+
+#include <cstdlib>
+
+using boost::asio::ip::tcp;
+
+//----------------------------------------------------------------------
+
+using chat_message_queue = std::deque<chat_message>;
+
+//----------------------------------------------------------------------
+// 基类，抽象类
+class chat_participant {
+public:
+  virtual ~chat_participant() {}
+  virtual void deliver(const chat_message &msg) = 0;
+};
+
+using chat_participant_ptr = std::shared_ptr<chat_participant>;
+
+//----------------------------------------------------------------------
+
+class chat_room {
+public:
+  // 加入聊天室
+  void join(chat_participant_ptr participant) {
+    participants_.insert(participant);
+    //std::cout << "插入聊天室！\n";
+    for (const auto &msg : recent_msgs_)
+      participant->deliver(msg);
+  }
+
+  // 离开聊天室
+  void leave(chat_participant_ptr participant) {
+    participants_.erase(participant);
+  }
+
+  // 分发消息
+  void deliver(const chat_message &msg) {
+    recent_msgs_.push_back(msg);
+    while (recent_msgs_.size() > max_recent_msgs)
+      recent_msgs_.pop_front();
+
+    for (auto &participant :
+         participants_) // 智能指针拷贝消耗大概是普通指针的20倍左右
+      participant->deliver(msg);
+  }
+
+private:
+  std::set<chat_participant_ptr> participants_;
+  enum { max_recent_msgs = 100 }; // 包存最近100条消息
+  chat_message_queue recent_msgs_;
+};
+
+//----------------------------------------------------------------------
+// 客户端会话类
+class chat_session : public chat_participant,
+                     public std::enable_shared_from_this<chat_session> {
+public:
+  chat_session(tcp::socket socket, chat_room &room)
+      : socket_(std::move(socket)), room_(room) {}
+
+  void start() {
+    room_.join(shared_from_this()); // 向聊天室加入一个客户端
+    do_read_header();
+  }
+
+  void deliver(const chat_message &msg) {
+    // 第一次是false
+    bool write_in_progress = !write_msgs_.empty();
+    write_msgs_.push_back(msg);
+    if (!write_in_progress) {
+      // 第一次
+      do_write();
+    }
+  }
+
+private:
+  void do_read_header() {
+    auto self(shared_from_this()); // 防止指针被析构
+    boost::asio::async_read(
+        socket_,
+        boost::asio::buffer(read_msg_.data(), chat_message::header_length),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec && read_msg_.decode_header()) {
+            //std::cout << "读取包头成功！\n";
+            do_read_body();
+          } else {
+            //std::cout << "error: " << ec << std::endl;
+            std::cout << "Player leave the room\n";
+            room_.leave(shared_from_this());
+          }
+        });
+  }
+
+  void do_read_body() {
+    auto self(shared_from_this());
+    //std::cout << "进入do read body！" << std::endl;
+    boost::asio::async_read(
+        socket_, 
+        boost::asio::buffer(read_msg_.body(), read_msg_.body_length()),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+          //std::cout << "进入处理包体回调！" << std::endl;
+          if (!ec) {
+            //room_.deliver(read_msg_);
+            //std::cout << "读取包体成功！" << std::endl;
+            handleMessage();
+            do_read_header();
+          } else {
+            std::cout << "error: " << ec << std::endl;
+            room_.leave(shared_from_this());
+          }
+        });
+  }
+  // 模板对象
+  template <typename T> T toObject() {
+    T obj;
+    std::stringstream ss(std::string(read_msg_.body(),
+                         read_msg_.body() + read_msg_.body_length()));
+    boost::archive::text_iarchive oa(ss);
+    oa &obj;
+    return obj;
+  }
+
+  // 处理消息
+  void handleMessage() { 
+    if (read_msg_.type() == MT_BIND_NAME) {
+      /*SBindName bindName;
+      std::stringstream ss(read_msg_.body());
+      boost::archive::text_iarchive oa(ss);
+      oa &bindName;*/
+      
+      auto bindName = toObject<SBindName>();
+      m_name = bindName.bindName();
+    } else if (read_msg_.type() == MT_CHAT_INFO) {
+      /*SChatInfo chat;
+      std::stringstream ss(std::string(
+          read_msg_.body(), read_msg_.body() + read_msg_.body_length()));
+      boost::archive::text_iarchive oa(ss);
+      oa &chat;*/
+      auto chat = toObject<SChatInfo>();
+      m_chatInformation = chat.chatInformation();
+
+      auto rinfo = buildRoomInfo();
+      chat_message msg;
+      msg.setMessage(MT_ROOM_INFO, rinfo);
+      room_.deliver(msg);
+      
+    } else {
+      // not valid msg do nothing
+    }
+  }
+
+  void do_write() {
+    auto self(shared_from_this());
+    boost::asio::async_write(
+        socket_,
+        boost::asio::buffer(write_msgs_.front().data(),
+                            write_msgs_.front().length()),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+          if (!ec) {
+            write_msgs_.pop_front();
+            if (!write_msgs_.empty()) {
+              do_write();
+            }
+          } else {
+            room_.leave(shared_from_this());
+          }
+        });
+  }
+
+  tcp::socket socket_;
+  chat_room &room_;
+  chat_message read_msg_;
+  chat_message_queue write_msgs_;
+  std::string m_name;
+  std::string m_chatInformation;
+  std::string buildRoomInfo() const {
+    SRoomInfo roomInfo(m_name, m_chatInformation);
+    std::stringstream ss;
+    boost::archive::text_oarchive oa(ss);
+    oa &roomInfo;
+    return ss.str();
+  }
+};
+
+//----------------------------------------------------------------------
+// 服务器类
+class chat_server {
+public:
+  chat_server(boost::asio::io_context &io_context,
+              const tcp::endpoint &endpoint)
+      : acceptor_(io_context, endpoint) {
+    do_accept();
+  }
+
+private:
+  void do_accept() {
+    acceptor_.async_accept([this](boost::system::error_code ec,
+                                  tcp::socket socket) {
+      if (!ec) {
+        auto session = std::make_shared<chat_session>(std::move(socket), room_);
+        session->start();
+        std::cout << "客户端建立连接" << std::endl;
+      }
+
+      do_accept();
+    });
+  }
+
+  tcp::acceptor acceptor_;
+  chat_room room_;
+};
+
+//----------------------------------------------------------------------
+
+int main(int argc, char *argv[]) {
+  try {
+    if (argc < 2) {
+      std::cerr << "Usage: chat_server <port> [<port> ...]\n";
+      return 1;
+    }
+
+    boost::asio::io_context io_context;
+
+    std::list<chat_server> servers;
+    for (int i = 1; i < argc; ++i) {
+      tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[i]));
+      servers.emplace_back(io_context, endpoint);
+    }
+
+    io_context.run();
+  } catch (std::exception &e) {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+  system("pause");
+  return 0;
+}
